b64624ca9a28817c1186e37bd7c9ab2ac09c6ebd
Brian Chrzanowski <chrzanowskitest@gmail.com>
2017-08-27 04:40:02 -0400

    I finally have an NTLM/LM hasher in raw ANSI C!

--- src/main.c
+++ src/main.c
@@ -18,18 +18,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <ctype.h>
+#include <stdint.h>
 
-#include "ntlm.h"
+
+// #include "platform.h"
+
+#include "DES.h"
+#include "MD4.h"
+#include "MD5.h"
+#include "LMhash.h"
 
 /* maximum values */
-#define BUFFER_SIZE  80000000
-#define HASH_SIZE    33
+#define BUFFER_SIZE  1024
 #define MAX_WORD_LEN 14
-#define DB_NAME "passes.db"
 
 /* define functions */
 int get_buffer (char *dest, int dest_size, long *used, FILE *source);
@@ -38,117 +43,55 @@ void monkey_bars (char *buffer, int buff_size, long used, int thread_num);
 /* -------------------------------------------------------------------------- */
 int main(int argc, char **argv)
 {
-	char *buffer = malloc(BUFFER_SIZE);
-	if (buffer == NULL) {
-		fprintf(stderr, "You don't have enough memory...\n");
-		return 1;
-	}
-	memset(buffer, 0, BUFFER_SIZE);
+	int len, i, max;
+	char plain[BUFFER_SIZE];
+	unsigned char buffer[BUFFER_SIZE];
+	unsigned char dest[BUFFER_SIZE];
 
-	uint8_t finished = 1;
-	long used_bytes;
-	long i;
-	char dest[HASH_SIZE] = { 0 };
+	while (fgets((char *)buffer, BUFFER_SIZE, stdin) == (char *)buffer) {
+		len = (strlen((char *)buffer) > 14) ? 14 : strlen((char *)buffer);
 
-	while (finished) {
-		/* read into the buffer */
-		finished = (uint8_t)
-			get_buffer (buffer, BUFFER_SIZE, &used_bytes, stdin);
+		if ('\n' == buffer[len - 1])
+			buffer[--len] = '\0';
 
-		/* climb over the super awesome monkey bars */
-		// monkey_bars(buffer, BUFFER_SIZE, used_bytes, 4);
-		i = 0;
-		while (i < used_bytes) {
-			/* NTLM over the buffer */
-			NTLM_hash (dest, (buffer + i));
-			printf("%s:%s\n", dest, buffer + i);
-			memset(dest, 0, HASH_SIZE);
+		max = (len > 14) ? 14 : len;
+		buffer[max] = 0;
 
-			/* compare memory with the target hash */
+		/* copy to final buffer, to not lose plaintext */
+		memcpy(plain, buffer, strlen((char *)buffer));
 
-			/* get next string index */
-			i = i + strlen(buffer + i) + 1;
+		for (i = 0; i < max; i++)
+			dest[i] = toupper(buffer[i]);
+
+		/* do LMhashing */
+		auth_LMhash((unsigned char *)dest, (unsigned char *)dest, len);
+
+		printf("  LM:");
+		for (i = 0; i < 16; i++)
+			printf("%.2X", dest[i]);
+
+		printf(":%s\n", plain);
+
+		/* do NTLM hashing */
+		max = (len > (BUFFER_SIZE / 2) ? (BUFFER_SIZE / 2) : len);
+		buffer[(2 * max) - 1] = 0;
+
+		for (i = max - 1; i > 0; i--) {
+			buffer[(i * 2)] = buffer[i];
+			buffer[(i * 2) - 1] = 0;
 		}
 
-		/* clean the buffer */
-		memset(buffer, 0, used_bytes);
+		auth_md4Sum(dest, buffer, 2 * max);
+
+		printf("NTLM:");
+		for (i = 0; i < 16; i++)
+			printf("%.2X", dest[i]);
+		printf(":%s\n", plain);
+
+		memset(buffer, 0, BUFFER_SIZE);
+		memset(dest, 0, BUFFER_SIZE);
+		memset(plain, 0, BUFFER_SIZE);
 	}
 
 	return 0;
-}
-
-void monkey_bars (char *buffer, int buff_size, long used, int thread_num)
-{
-	long i;
-    char dest[HASH_SIZE];
-	char *ptrs[thread_num];
-	memset(dest, 0, HASH_SIZE);
-
-	/* find where each thread should begin execution */
-	long offset = used / thread_num;
-	for (i = 0; i < thread_num; i++) {
-		ptrs[i] = buffer + offset * i;
-		ptrs[i] = ptrs[i] + strlen(buffer + *ptrs[i]) + 1;
-	}
-
-	i = 0;
-	while (i < used) {
-
-		/* NTLM over the buffer */
-		NTLM_hash (dest, (buffer + i));
-		printf("%s:%s\n", dest, buffer + i);
-
-		/* compare memory with the target hash */
-
-		/* get next string index */
-		i = i + strlen(buffer + i) + 1;
-	}
-
-	return;
-}
-
-/* -------------------------------------------------------------------------- */
-
-/*
- * function : word_gen
- * purpose  : generate words for the password cracker to use
- * input    : char *dest          destination,
- *          : int dest_size       size of destination,
- *          : int *used           number of bytes used within the buffer
- *          : FILE *source        where the words to test are coming from,
- *	                          normally stdin or such. This also leaves
- *	                          it open to simply read straight from a file
- * output   : int       0 -> EOF
- *
- * NOTE : (probably) NOT thread safe
- *
- * TODO
- *	Make buffer caching quicker (remove if statements)
- */
-
-int get_buffer (char *dest, int dest_size, long *used, FILE *source)
-{
-	int i, c, t;
-	
-	/* when set to '1', it is safe to leave the loop */
-	t = 0;
-
-        for (i = 0; (c = getc(source)) != EOF; i++) {
-		/* straight copy the char from the source into dest */
-		if (c == '\n') {
-			*(dest + i) = '\0';
-			t = 1;
-		} else {
-			*(dest + i) = (char)c;
-			t = 0;
-		}
-
-		/* check if we need to leave the loop */
-		if (i >= dest_size - 100 && t == 1)
-			break;
-	}
-
-	*used = i;
-
-	return (c == EOF) ? 0 : 1;
 }
