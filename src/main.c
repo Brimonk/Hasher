98e90d54b5317f4f861d02783008b42b83df1b53
brimonk <bflatmaster@gmail.com>
2017-08-28 03:32:38 -0400

    compartmentalized some repetitive code. Command line arguments are next.

--- src/main.c
+++ src/main.c
@@ -36,17 +36,24 @@
 #define BUFFER_SIZE  1024
 #define MAX_WORD_LEN 14
 
-/* define functions */
-int get_buffer (char *dest, int dest_size, long *used, FILE *source);
-void monkey_bars (char *buffer, int buff_size, long used, int thread_num);
+void print_to_stdout(char *type, unsigned char *hash, char *plaintext);
+void buff_to_upper(unsigned char *dest, unsigned char *source, int max);
+void char_ptrs_clear(char **ptrs, int length);
 
 /* -------------------------------------------------------------------------- */
 int main(int argc, char **argv)
 {
 	int len, i, max;
 	char plain[BUFFER_SIZE];
+
+	char *ptrs[16] = {0};
+	
 	unsigned char buffer[BUFFER_SIZE];
 	unsigned char dest[BUFFER_SIZE];
+
+	ptrs[0] = (char *)&buffer[0];
+	ptrs[1] = (char *)&dest[0];
+	ptrs[2] = (char *)&plain[0];
 
 	while (fgets((char *)buffer, BUFFER_SIZE, stdin) == (char *)buffer) {
 		len = (strlen((char *)buffer) > 14) ? 14 : strlen((char *)buffer);
@@ -60,17 +67,11 @@ int main(int argc, char **argv)
 		/* copy to final buffer, to not lose plaintext */
 		memcpy(plain, buffer, strlen((char *)buffer));
 
-		for (i = 0; i < max; i++)
-			dest[i] = toupper(buffer[i]);
+		buff_to_upper(&dest[0], &buffer[0], max);
 
 		/* do LMhashing */
 		auth_LMhash((unsigned char *)dest, (unsigned char *)dest, len);
-
-		printf("  LM:");
-		for (i = 0; i < 16; i++)
-			printf("%.2X", dest[i]);
-
-		printf(":%s\n", plain);
+		print_to_stdout("  LM", &dest[0], plain);
 
 		/* do NTLM hashing */
 		max = (len > (BUFFER_SIZE / 2) ? (BUFFER_SIZE / 2) : len);
@@ -82,16 +83,45 @@ int main(int argc, char **argv)
 		}
 
 		auth_md4Sum(dest, buffer, 2 * max);
+		print_to_stdout("NTLM", &dest[0], plain);
 
-		printf("NTLM:");
-		for (i = 0; i < 16; i++)
-			printf("%.2X", dest[i]);
-		printf(":%s\n", plain);
-
-		memset(buffer, 0, BUFFER_SIZE);
-		memset(dest, 0, BUFFER_SIZE);
-		memset(plain, 0, BUFFER_SIZE);
+		char_ptrs_clear(&ptrs[0], 3);
 	}
 
 	return 0;
+}
+
+void char_ptrs_clear(char **ptrs, int length)
+{
+	int i;
+
+	for (i = 0; i < length; i++) {
+		memset(ptrs[i], 0, BUFFER_SIZE);
+	}
+}
+
+void print_to_stdout(char *type, unsigned char *hash, char *plaintext)
+{
+	/* make minimal syscalls. Use buffers for performance! */
+	char buf[BUFFER_SIZE];
+	int i, j;
+
+	sprintf(&buf[0], "%s:", type);
+
+	for (j = 0, i = strlen(type) + 1; j < 16; i += 2, j++) {
+		sprintf(&buf[i], "%.2X", (unsigned char)hash[j]);
+	}
+
+	sprintf(&buf[i], ":%s\n", plaintext);
+
+	printf("%s", buf);
+}
+
+void buff_to_upper(unsigned char *dest, unsigned char *source, int max)
+{
+	/* converts all bytes in buffer to upper case */
+	int i;
+
+	for (i = 0; i < max; i++)
+		dest[i] = toupper(source[i]);
 }
